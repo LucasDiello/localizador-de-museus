36fb3564f1f6674283aa71a5a3f8869a30ef5390
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2024-01-31 13:17:20 -0300

    feat:4 - Implement the service layer getClosestMuseum method and tests validation method

--- src/main/java/com/betrybe/museumfinder/service/MuseumService.java
+++ src/main/java/com/betrybe/museumfinder/service/MuseumService.java
@@ -2,34 +2,53 @@ package com.betrybe.museumfinder.service;
 
 import com.betrybe.museumfinder.database.MuseumFakeDatabase;
 import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.exception.MuseumNotFoundException;
 import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.util.CoordinateUtil;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 /**
- * * Esta classe representa o serviço de gestão de museus.
+ * Esta classe representa o serviço de gestão de museus.
+ *
  */
 @Service
 public class MuseumService implements MuseumServiceInterface {
 
   private final MuseumFakeDatabase fakedb;
 
+
   public MuseumService(MuseumFakeDatabase fakedb) {
     this.fakedb = fakedb;
   }
 
+  /**
+   * Lança uma exceção se a validação da coordenada falhar.
+   *
+   * @param validateCoordinate Se a validação da coordenada falhou.
+   */
+  public void throwError(boolean validateCoordinate) {
+    if (!validateCoordinate) {
+      throw new InvalidCoordinateException();
+    }
+  }
+
   @Override
   public Museum getClosestMuseum(Coordinate coordinate, Double maxDistance) {
-    return null;
+    boolean coordinateValid = CoordinateUtil.isCoordinateValid(coordinate);
+    throwError(coordinateValid);
+    Optional<Museum> closestMuseum = fakedb.getClosestMuseum(coordinate, maxDistance);
+    if (closestMuseum.isEmpty()) {
+      throw new MuseumNotFoundException();
+    }
+    return closestMuseum.get();
   }
 
   @Override
   public Museum createMuseum(Museum museum) {
-    boolean validateCoordinate = CoordinateUtil.isCoordinateValid(museum.getCoordinate());
-    if (!validateCoordinate) {
-      throw new InvalidCoordinateException();
-    }
+    boolean coordinateValid = CoordinateUtil.isCoordinateValid(museum.getCoordinate());
+    throwError(coordinateValid);
     return fakedb.saveMuseum(museum);
   }
 
