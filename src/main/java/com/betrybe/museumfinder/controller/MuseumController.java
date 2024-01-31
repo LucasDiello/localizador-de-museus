5e8ee7c980d8847501af4b6a36ed31d8001b0c77
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2024-01-31 15:06:24 -0300

    feat:6 - Create route GET /museums/closest

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -1,14 +1,18 @@
 package com.betrybe.museumfinder.controller;
 
 import com.betrybe.museumfinder.dto.MuseumCreationDto;
+import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
 import com.betrybe.museumfinder.util.ModelDtoConverter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -28,5 +32,22 @@ public class MuseumController {
   public ResponseEntity<Museum> newMuseum(@RequestBody MuseumCreationDto newMuseum) {
     Museum savedMuseum = museumService.createMuseum(ModelDtoConverter.dtoToModel(newMuseum));
     return ResponseEntity.status(HttpStatus.CREATED).body(savedMuseum);
+  }
+
+  /**
+   * Endpoint para buscar o museu mais próximo com base em coordenadas e distância máxima.
+   *
+   * @param lat           Latitude da coordenada inicial.
+   * @param lng           Longitude da coordenada inicial.
+   * @param maxDist   Distância máxima em quilômetros para a busca do museu mais próximo.
+   */
+  @GetMapping("/closest")
+  public ResponseEntity<MuseumDto> searchClosestMuseums(
+      @RequestParam double lat,
+      @RequestParam double lng,
+      @RequestParam("max_dist_km") double maxDist) {
+    Coordinate coordinate = new Coordinate(lat, lng);
+    Museum closestMuseum = museumService.getClosestMuseum(coordinate, maxDist);
+    return ResponseEntity.ok(ModelDtoConverter.modelToDto(closestMuseum));
   }
 }
